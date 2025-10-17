375b1cc39a746d748137865e5a6bb90d5cf1b21a
sonvtnq <sonvt.np@gmail.com>
2025-10-17 23:28:41 +0700

    .env

--- src/main/java/com/billard/BillardRankings/BillardRankingsApplication.java
+++ src/main/java/com/billard/BillardRankings/BillardRankingsApplication.java
@@ -15,6 +15,16 @@ public class BillardRankingsApplication {
 		System.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));
 		System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
 		System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
+
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
+		System.setProperty("JWT_REFRESH_EXPIRATION", dotenv.get("JWT_REFRESH_EXPIRATION"));
+
+
 		SpringApplication.run(BillardRankingsApplication.class, args);
 	}
 
