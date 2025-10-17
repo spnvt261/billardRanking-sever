c1beb2d03f421ee96265440f11ce3d9e5322eb2e
sonvtnq <sonvt.np@gmail.com>
2025-10-17 19:56:15 +0700

    cloudinary key

--- src/main/java/com/billard/BillardRankings/config/CloudinaryConfig.java
+++ src/main/java/com/billard/BillardRankings/config/CloudinaryConfig.java
@@ -1,17 +1,29 @@
 package com.billard.BillardRankings.config;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import java.util.Map;
+
 @Configuration
 public class CloudinaryConfig {
+
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
         Map<String, String> values = ObjectUtils.asMap(
-                "cloud_name", "djeohgclg",
-                "api_key", "778676216696754",
-                "api_secret", "cMk7USAzkYsH436XCB77yc3acN8"
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret
         );
         return new Cloudinary(values);
     }
