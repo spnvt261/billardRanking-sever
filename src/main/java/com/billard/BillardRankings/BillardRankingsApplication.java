c6796cca84da303f42349ccae9f1af3247f4ede7
sonvtnq <sonvt.np@gmail.com>
2025-10-17 20:54:22 +0700

    .env

--- src/main/java/com/billard/BillardRankings/BillardRankingsApplication.java
+++ src/main/java/com/billard/BillardRankings/BillardRankingsApplication.java
@@ -1,5 +1,6 @@
 package com.billard.BillardRankings;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,6 +9,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class BillardRankingsApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load(); // tự động đọc file .env ở project root
+
+		// Thiết lập biến hệ thống để Spring Boot có thể đọc
+		System.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));
+		System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
+		System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
 		SpringApplication.run(BillardRankingsApplication.class, args);
 	}
 
